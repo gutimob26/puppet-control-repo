8038a7cf4a0115d6523348ec4b84b505707e2ba2
gutimob26 <gutimob26@gmail.com>
2016-09-14 14:42:17 -0600

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,26 @@
+# This is a profile base that does stuff
 class profile::base {
-include apache
-include tomcat
+  user { 'root':
+    ensure   => 'present',
+    comment  => 'root',
+    gid      => '0',
+    home     => '/root',
+    password => '$1$Tb9hAyyD$iWQhIMeYbBg7ES6vINn6H.',
+    shell    => '/bin/bash',
+    uid      => '0',
+  }
+
+  user { 'laura':
+    ensure     => present,
+    managehome => true,
+  }
+
+  include ntp
+
+  include ::epel
+
+  package { 'vim-enhanced':
+    ensure => installed,
+  }
+
 }
